81a5a11c356e55e7b5a4810777ac54622585b7a5
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2024-02-12 18:06:49 +0530

    Lazy loading is impleneted in lazyloadingmodule.js file

--- wd401/src/index.js
+++ wd401/src/index.js
@@ -1,7 +1,15 @@
 import "./css/styles.css";
+import './lazymodule.js';
 
 
-console.log("Hello World!");
 
-// we are gonna create a pyramid
-let i = 10;
+document.getElementById('lazy-button').addEventListener('click', () => {
+import('./lazyModule. js') . then ((module) => {
+    module.default();
+}
+);
+}
+);
+    
+
+console. log('Hello world');
